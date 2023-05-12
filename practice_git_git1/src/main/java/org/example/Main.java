026b799ad9afe27a851ed174eded5b64f717cbe4
DilbaraAsanalieva <dilbara2025@gmail.com>
2023-05-12 13:22:03 -0500

    some changes

---
+++ practice_git_git1/src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public static void welcome(){
+        System.out.println("Welcome to Amsterdam, Netherlands!");
+    }
+
+    public static void hello( ){
+        System.out.println("Hello from Ireland");
+
+    }
+    public static void block(){
+        System.out.println("Block of code");
+        System.out.println("Wall Street of NYC");
+    }
+
+
+
+}
